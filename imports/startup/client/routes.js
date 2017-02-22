a75585be4e00bcc716eca07f2f253feda2afda0a
Prawee Wongsa <prawee@hotmail.com>
2017-02-22 20:42:59 +0700

    added default route

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,8 +1,30 @@
 console.log('routes.js of client is loaded.');
+/**
+ * import package
+ */
+// import { FlowRouter } from 'meteor/kadira:flow-router';
+// import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 /**
  * import templates
  */
 import '/imports/ui/layouts/app';
+import '/imports/ui/layouts/header';
+
+import '/imports/ui/components/users/header';
 
 
+/**
+ * route definitions
+ */
+BlazeLayout.setRoot('body');
+FlowRouter.route('/', {
+    name: 'app.home',
+    triggersEnter: [AccountsTemplates.ensureSignedIn],
+    action() {
+        BlazeLayout.render('defaultLayout', {
+            header: 'header',
+            content: 'message'
+        });
+    }
+});
